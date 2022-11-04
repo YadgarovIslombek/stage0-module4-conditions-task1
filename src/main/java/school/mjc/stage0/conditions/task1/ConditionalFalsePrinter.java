5849f142ed4ca751afb17b605716da66013b656c
Islombek Yadgarov <islam.yadgarov.677@gmail.com>
2022-11-04 11:37:25 +0500

    "brr"

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        boolean a = false;
+        if(a){
+            System.out.println(a);
+        }else {
+            System.out.println(a);
+
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        boolean a = true;
+        if(a){
+            System.out.println(a);
+        }else {
+            System.out.println(a);
+
+        }
     }
 }
